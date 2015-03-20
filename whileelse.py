611d17bca109ab5e3470c226465493a6c112bb9c
Chandrakant Gopalan <chandrakant.gopalan@gmail.com>
2015-03-20 16:22:18 -0400

    Added break condition

--- whileelse.py
+++ whileelse.py
@@ -1,9 +1,12 @@
-""" You can have an else clause with a while. Works like for-else. """
+""" You can have an else clause with a while. Works like for-else. 
+    When break is encountered, it exits the loop without executing else. """
 
 i = 5
 
 while i > 1:
   print("Whil-ing away!")
   i -= 1
+  if == 3:
+    break
 else:
   print("Finished up!")
