735c909f1f3cdf4dae9f048014dd6a6a68d0ddf3
David Taylor <prooffreader@gmail.com>
2015-03-20 06:06:41 -0400

    Revert "added concatenatestrings"
    This reverts commit beb1ba30f7038f63f4a1513a87c7068bb85de613.

--- concatenatestrings.py
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/env python3
-"""Concatenate strings to elegantly go over line breaks in code"""
-
-my_long_text = ("We are no longer the knights who say Ni! "
-                "We are now the knights who say ekki-ekki-"
-                "ekki-p'tang-zoom-boing-z'nourrwringmm!")
