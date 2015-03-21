205a18bec096a525ebf9af13309018cd1e6cb26c
ana-balica <ana.balica@gmail.com>
2015-03-21 09:39:59 +0530

    replace sum function with product

--- argumentunpacking.py
+++ argumentunpacking.py
@@ -1,10 +1,10 @@
 #! /usr/bin/env python3
 """simple tuple and dictionary unpacking"""
-def sum(a, b):
-    return a + b
-    
+def product(a, b):
+    return a * b
+
 argument_tuple = (1, 1)
 argument_dict = {'a': 1, 'b': 1}
 
-print(sum(*argument_tuple))
-print(sum(**argument_dict))
+print(product(*argument_tuple))
+print(product(**argument_dict))
