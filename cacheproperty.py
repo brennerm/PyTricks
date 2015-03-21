b5a17fbe24dc2d179556f7a8d319cf4dd70755f5
Max Brenner <xamrennerb@gmail.com>
2015-03-21 18:23:26 +0100

    added contribution of Kzinglzy

---
+++ cacheproperty.py
@@ -0,0 +1,29 @@
+class PropertyCache:
+    """ a decorator to cache property
+    """
+
+    def __init__(self, func):
+        self.func = func
+
+    def __get__(self, obj, cls):
+        if not obj:
+            return self
+        value = self.func(obj)
+        setattr(obj, self.func.__name__, value)
+        return value
+        
+
+class Foo:
+    def __init__(self):
+        self._property_to_be_cached = 'result'
+
+    @PropertyCache
+    def property_to_be_cached(self):
+        print('compute')
+        return self._property_to_be_cached
+    
+test = Foo()
+
+print(test.property_to_be_cached)
+print(test.property_to_be_cached)
+
