08cd09f16db163940233e246645a39cf996b5bec
illuz <iilluzen@gmail.com>
2015-03-22 00:19:32 +0800

    fix comment

--- controlwhitespaces.py
+++ controlwhitespaces.py
@@ -1,10 +1,10 @@
 #! /usr/bin/env python3
-"""control the whitespaces in sing"""
+"""control the whitespaces in string"""
 
 s = 'The Little Price'
 
-# justify stirng to be at least width wide
-# by adding whitespace
+# justify string to be at least width wide
+# by adding whitespaces
 width = 20
 s1 = s.ljust(width)
 s2 = s.rjust(width)
@@ -13,7 +13,7 @@ print(s1)   # 'The Little Price    '
 print(s2)   # '    The Little Price'
 print(s3)   # '  The Little Price  '
 
-# strip whitespace in two sides of sing
+# strip whitespaces in two sides of string
 print(s3.lstrip())  # 'The Little Price  '
 print(s3.rstrip())  # '  The Little Price'
 print(s3.strip())   # 'The Little Price'
