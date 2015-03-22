6bb426309993c7650c6567c1a7bf62d37e99f52e
parasimha <pcunningham80@gmail.com>
2015-03-22 10:38:03 +0000

    Update dictsortbyvalue.py

--- dictsortbyvalue.py
+++ dictsortbyvalue.py
@@ -3,3 +3,18 @@
  
 d = {'apple': 10, 'orange': 20, 'banana': 5, 'rotten tomato': 1}
 print (sorted(d.items(), key = lambda x: x[1]))
+
+
+""" Sort using operator.itemgetter as the sort key instead of a lambda"""
+
+
+from operator import itemgetter
+
+
+print(sorted(d.items(), key=itemgetter(1)))
+
+
+"""Sort dict keys by value"""
+
+
+print(sorted(d, key=d.get))
