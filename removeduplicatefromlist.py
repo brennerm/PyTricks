a57cb5501d86d70200639a3f484f4d902b091bb9
Talha Oz <oztalha@users.noreply.github.com>
2015-03-22 07:15:08 -0400

    comment was missing
    order not preserved info added to the comment

--- removeduplicatefromlist.py
+++ removeduplicatefromlist.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python3
-"""remove duplicate items from list"""
+"""remove duplicate items from list. note: does not preserve the original list order"""
 
 items = [1, 2, 2, 3, 3, 3]
 
