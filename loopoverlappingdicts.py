aeae66024058a1e5569eb6eef1f9a9cd656b3f3d
Raúl Cumplido <raulcumplido@gmail.com>
2015-03-23 13:45:56 +0000

    Add example of ovelapping (key:value) on dicts for Python3

--- loopoverlappingdicts.py
+++ loopoverlappingdicts.py
@@ -2,7 +2,7 @@
 """loop over dicts that share (some) keys in Python2"""
 
 dctA = {'a': 1, 'b': 2, 'c': 3}
-dctB = {'b': 4, 'c': 5, 'd': 6}
+dctB = {'b': 4, 'c': 3, 'd': 6}
 
 for ky in set(dctA) & set(dctB):
     print(ky)
@@ -10,3 +10,7 @@ for ky in set(dctA) & set(dctB):
 """loop over dicts that share (some) keys in Python3"""
 for ky in dctA.keys() & dctB.keys():
     print(ky)
+
+"""loop over dicts that share (some) keys and values in Python3"""
+for item in dctA.items() & dctB.items():
+    print(item)
