2238599bf29cdcdddea67d465f49314b3bb61d80
sgs00 <g.saviano@gmail.com>
2015-04-16 00:37:22 +0200

    pep8 (two blank lines before def), added conditional call through dictionary

--- conditionalfunctioncall.py
+++ conditionalfunctioncall.py
@@ -1,10 +1,20 @@
 #! /usr/bin/env python3
 """calling different functions with same arguments based on condition"""
+
+
 def product(a, b):
     return a * b
+
 
 def subtract(a, b):
     return a - b
 
 b = True
-print((product if b else subtract)(1, 1))
+
+function = product if b else subtract
+result = function(1, 1)
+print(result)
+
+function = {True: product, False: subtract}[b]
+result = function(1, 1)
+print(result)
