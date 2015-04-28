171432ea007b3ad179deb8133a08b86c183d9221
Pahaz Blinov <pahaz.blinov@gmail.com>
2015-04-28 19:44:35 +0500

    style(keydefaultdict.py): PEP8 style

--- keydefaultdict.py
+++ keydefaultdict.py
@@ -3,16 +3,18 @@ keydefaultdict with where the function recieves the key.
 """
 from collections import defaultdict
 
+
 class keydefaultdict(defaultdict):
     def __missing__(self, key):
         if self.default_factory is None:
-            raise KeyError( key )
+            raise KeyError(key)
         else:
             ret = self[key] = self.default_factory(key)
             return ret
 
+
 def pow2(n):
-	return 1 << n 
+    return 1 << n 
 
 d = keydefaultdict(pow2)
 print(d[1])
