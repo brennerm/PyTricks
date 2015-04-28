ff425b14583a827010a50f9e7908773a41b706f2
Pahaz Blinov <pahaz.blinov@gmail.com>
2015-04-28 19:35:51 +0500

    style(dictsortbyvalue.py): PEP8 style

--- dictsortbyvalue.py
+++ dictsortbyvalue.py
@@ -2,7 +2,7 @@
 """ Sort a dictionary by its values with the built-in sorted() function and a 'key' argument. """
  
 d = {'apple': 10, 'orange': 20, 'banana': 5, 'rotten tomato': 1}
-print (sorted(d.items(), key = lambda x: x[1]))
+print(sorted(d.items(), key=lambda x: x[1]))
 
 
 """ Sort using operator.itemgetter as the sort key instead of a lambda"""
