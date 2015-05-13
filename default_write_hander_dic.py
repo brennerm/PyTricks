1888bf8cc80dff125fea4c4a6b301c7ad0d63015
Hu Wenchao <zjuhwc@gmail.com>
2015-05-13 15:02:30 +0800

    update default_write_hander_dic.py

--- default_write_hander_dic.py
+++ default_write_hander_dic.py
@@ -2,15 +2,15 @@
 # -*- coding: UTF-8 -*-
 '''
 Auther      : Hu Wenchao
-Description :
+Description : Form a data structure like defaultdict, the key is path of file,
+                and return handler of the file. If there is no handler for the
+                file, create one and return. If this way, we do not need to open
+                and close the file again and again, also we can use try/finally
+                statements to ensure all the files are closed.
 '''
 from collections import defaultdict
 
 class keydefaultdict(defaultdict):
-    '''构造一个数据结构，类似defaultdict，传入文件路径，返回对应的写文件handler。
-    如果不存在则创建一个。在所有操作阶数后，关闭改字典中所有的文件。
-    这样可以避免反复开关文件的开销，且可以进行容错处理，保证文件全都关闭。
-    '''
     def __missing__(self, key):
         if self.default_factory is None:
             raise KeyError(key)
@@ -23,9 +23,17 @@ def file_write_handler(path):
 
 file_handler_dic = keydefaultdict(file_write_handler)
 
+# write number from 1 to 10000 to files depending on its remainder.
+
+# the direct way. We need to open and close files for 10000 times.
+for i in range(10000):
+    with open('log%02d' % (i % 10), 'a') as f:
+        f.write('%d\n' % i)
+
+# The way use defaultdict. Open the file when needed, and close them at last.
 try:
-    for i in range(1000):
-        file_handler_dic['log%02d' % (i % 10)].write('%04d\n' % i)
+    for i in range(10000):
+        file_handler_dic['log%02d' % (i % 10)].write('%d\n' % i)
 finally:
     for f in file_handler_dic.values():
         f.close()
