bddd7374e77d11ed5a613c09d55cae81346c85ed
Flint <flint@forge.systems>
2018-01-26 17:13:47 +0100

    ifelse comment switch

--- commentblock_onecharswitch.py
+++ commentblock_onecharswitch.py
@@ -3,12 +3,28 @@
 comment blocks can be turn on and off 
 in one character switch when preceded by #"""
 
-#"""
+#'''
 print('see me ?')
-#"""
+#'''
 
-"""
+'''
 print('see me too ?')
-#"""
+#'''
 
-print('continue')
+print('there is more')
+
+""" 
+This applies to turn either one block as comments
+OR the next one.
+
+In the following example, removing the first #
+will comment block 1 and decomment block 2
+"""
+
+#'''
+print('block 1')
+'''
+print('or block 2 ?')
+#'''
+
+print('nice!')
