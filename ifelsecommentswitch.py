920159520dce33cc4f60374392555dc136be1d31
Flint <flint@forge.systems>
2018-02-01 09:48:40 +0100

    ifelse comments switch

--- ifelsecommentswitch.py
+++ ifelsecommentswitch.py
@@ -4,9 +4,10 @@ comment blocks can be turn on and off
 in one character switch when preceded by #"""
 
 #''' #< toggling off this # turns the block as a comment (it wasn't, line was mute)
-print("you saw me, now you don't see me")
+print("you saw me!")
 #''' 
 
+print("catched!")
 
 """
 second example is the equivalent 
@@ -18,3 +19,5 @@ print("you saw me now you don't see me")
 '''# which will stop here, where it was previously beginning, hence turning it in the ending block tag
 print("you didn't see me, now you do !")
 #''' # and so, it uncomments the one below
+
+print('continue...')
