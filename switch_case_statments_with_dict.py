52b1c93dca8bc46a6acbe22d2440be34141b12f2
Kazuhiko <leu_k@hotmail.com>
2018-03-06 10:01:09 -0300

    Switch/Case statments using dictionaries.

---
+++ switch_case_statments_with_dict.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python3
+
+"""Switch/Case statments with Dictionaries 
+   Based on Dan Bader video: https://www.youtube.com/watch?v=gllUwQnYVww
+"""
+
+def traditional(operator, x , y):
+	if operator == 'add':
+		return x + y
+	elif operator == 'sub':
+		return x - y
+	elif operator == 'mul':
+		return x * y
+	elif operator == 'div':
+		return x / y
+
+
+def with_dict(operator, x ,y):
+	return {
+		'add' : lambda: x + y,
+		'sub' : lambda: x - y,
+		'mul' : lambda: x * y,
+		'div' : lambda: x / y,
+	}.get(operator, lambda: Nome)()
