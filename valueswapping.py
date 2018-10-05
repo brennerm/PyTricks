dc2d1e1287cbed5143624cc5a49918e84d8984f7
razinc <l_noir@yahoo.com>
2018-10-06 00:41:21 +0800

    Added example for list type of variable.

--- valueswapping.py
+++ valueswapping.py
@@ -1,6 +1,11 @@
 #! /usr/bin/env python3
-"""pythonic way of value swapping"""
+# Type of variable: Number
 a, b = 5, 10
 print(a, b)
 a, b = b, a
 print(a, b)
+# Type of variable: List
+myList = [1, 2, 3, 4, 5]
+print("Initial Array :", myList)
+myList[0], myList[1] = myList[1], myList[0]
+print("Swapped Array :", myList)
